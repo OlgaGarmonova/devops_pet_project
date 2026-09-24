4645c9b17b74d0dff85f99d868a3443232d9a6c9
OlgaGarmonova <o.v.garmonova@gmail.com>
2026-09-24 13:07:45 +0200

    uniting

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,111 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "pet-project-vpc"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "pet-project-public-subnet"
+    Environment = "dev"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "pet-project-igw"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "pet-project-public-rt"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+#Security Group
+resource "aws_key_pair" "deployer" {
+  key_name   = "devops-pet-project-key"
+  public_key = file("~/.ssh/devops-pet-project.pub")
+
+  tags = {
+    Name        = "pet-project-ssh-key"
+    Environment = "dev"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "pet-project-web-sg"
+  description = "Allow HTTP, HTTPS and restricted SSH traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from anywhere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "pet-project-web-sg"
+    Environment = "dev"
+  }
+}
